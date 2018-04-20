0146333679f35036111df4db45254ae55f1a752c
Farkas, Istvan <Istvan.Farkas@kronsoft.ro>
2018-04-20 11:41:26 +0300

    0 Work hour difference

--- src/main/java/ro/wolfnet/programmanager/service/generate/VacationRule.java
+++ src/main/java/ro/wolfnet/programmanager/service/generate/VacationRule.java
@@ -34,12 +34,14 @@ import static java.util.stream.Collectors.toCollection;
  */
 @Component
 public class VacationRule implements GenerateRule {
-	
-	@Autowired
-	private StationRepository stationRepository;
-	
-	@Autowired
-	private RuleRepository ruleRepository;
+
+  /** The station repository. */
+  @Autowired
+  private StationRepository stationRepository;
+
+  /** The rule repository. */
+  @Autowired
+  private RuleRepository ruleRepository;
 
   /* (non-Javadoc)
    * @see ro.wolfnet.programmanager.service.generate.GenerateRule#filterEmployees(long, java.util.Date, java.util.List, java.util.List)
@@ -50,7 +52,7 @@ public class VacationRule implements GenerateRule {
     if (vacationRules == null || vacationRules.size() == 0) {
       return employees;
     }
-    
+
     List<EmployeeStatusModel> availableEmployees = new ArrayList<>();
     for (EmployeeStatusModel employee : employees) {
       EmployeeStatusModel availableEmployee = getAvailableEmployee(date, employee, vacationRules);
@@ -61,168 +63,261 @@ public class VacationRule implements GenerateRule {
     return availableEmployees;
   }
 
+  /**
+   * Gets the available employee.
+   *
+   * @param date the date
+   * @param employee the employee
+   * @param vacationRules the vacation rules
+   * @return the available employee
+   */
   private EmployeeStatusModel getAvailableEmployee(Date date, EmployeeStatusModel employee, List<RuleVacationEntity> vacationRules) {
-	List<RuleVacationEntity> myVacations = getVacationsOfEmployee(employee.getId(), vacationRules);
-	if (myVacations == null || myVacations.size() == 0) {
-		return employee;
-	}
-	
+    List<RuleVacationEntity> myVacations = getVacationsOfEmployee(employee.getId(), vacationRules);
+    if (myVacations == null || myVacations.size() == 0) {
+      return employee;
+    }
+
     for (RuleVacationEntity vacation : myVacations) {
-        if (vacation.getStart().after(date) || vacation.getEnd().before(date)) {
-          employee.setWorkedHours(employee.getWorkedHours() + getAuxWorkedHours(date, employee, myVacations, vacationRules));
-          continue;
-        }
-        //employee is on vacation
-        return null;
+      if (vacation.getStart().after(date) || vacation.getEnd().before(date)) {
+        employee.setWorkedHours(employee.getWorkedHours() + getAuxWorkedHours(date, employee, vacation, myVacations, vacationRules));
+        continue;
+      }
+      //employee is on vacation
+      return null;
     }
     return employee;
   }
 
-  private List<RuleVacationEntity> getVacationsOfEmployee(long employeeId, List<RuleVacationEntity> vacationRules) {
-	if (employeeId == 0 || vacationRules == null || vacationRules.size() == 0) {
-		return null;
-	}
-	
-	List<RuleVacationEntity> myVacations = new ArrayList<>();
-	for (RuleVacationEntity vacation:vacationRules) {
-		if (vacation.getEmployees().iterator().next().getId() == employeeId) {
-			myVacations.add(vacation);
-		}
-	}
-	return myVacations;
-}
+  /**
+   * Gets the aux worked hours.
+   *
+   * @param date the date
+   * @param employee the employee
+   * @param myCurrentVacation the my current vacation
+   * @param myVacations the my vacations
+   * @param vacationRules the vacation rules
+   * @return the aux worked hours
+   */
+  private double getAuxWorkedHours(Date date, EmployeeStatusModel employee, RuleVacationEntity myCurrentVacation, List<RuleVacationEntity> myVacations,
+                                   List<RuleVacationEntity> vacationRules) {
+    double auxWorkedHours = getAuxWorkedHoursFromDifferences(date, employee, myVacations, vacationRules);
+    if (auxWorkedHours == 0) {
+      auxWorkedHours = (Utils.getDateDifference(myCurrentVacation.getStart(), myCurrentVacation.getEnd(), TimeUnit.DAYS) * 8) /
+        Utils.getDayOfMonthNumbers(date);
+      auxWorkedHours *= Utils.getDateDifference(Utils.getDateFromBeginningOfMonth(date), date, TimeUnit.DAYS);
+    }
+    return auxWorkedHours;
+  }
 
-/**
+  /**
+   * Gets the vacations of employee.
+   *
+   * @param employeeId the employee id
+   * @param vacationRules the vacation rules
+   * @return the vacations of employee
+   */
+  private List<RuleVacationEntity> getVacationsOfEmployee(long employeeId, List<RuleVacationEntity> vacationRules) {
+    if (employeeId == 0 || vacationRules == null || vacationRules.size() == 0) {
+      return null;
+    }
+
+    List<RuleVacationEntity> myVacations = new ArrayList<>();
+    for (RuleVacationEntity vacation : vacationRules) {
+      if (vacation.getEmployees().iterator().next().getId() == employeeId) {
+        myVacations.add(vacation);
+      }
+    }
+    return myVacations;
+  }
+
+  /**
    * Gets the aux worked hours.
    * 
    * Rule: ((how many assigned stations I have * worked employees on each one / how many employees will work in this interval) - my vacation hours) /
    * work days number * work day index * -1
    *
+   * @param workDate the work date
+   * @param employee the employee
+   * @param vacationsOfEmployee the vacations of employee
+   * @param allVacations the all vacations
    * @return the aux worked hours
    */
-  private double getAuxWorkedHours(Date workDate, EmployeeStatusModel employee, List<RuleVacationEntity> vacationsOfEmployee, List<RuleVacationEntity> allVacations) {
-	  if (workDate == null || vacationsOfEmployee == null || vacationsOfEmployee.size() == 0) {
-		  return 0;
-	  }
-	  
-	  double workedHoursDifferenceBetweenMeAndOthers = 0;
-	  Date start = Utils.getDateFromBeginningOfMonth(workDate);
-	  Date end = Utils.getDateAtEndOfMonth(workDate);
-	  List<Date> vacationDays = new ArrayList<>();
-	  List<StationEntity> employeeStations = stationRepository.getStationsOfEmployee(employee.getId());
-	  for (RuleVacationEntity vacation:vacationsOfEmployee) {
-		  Date calcStart = Utils.getMaximum(start, vacation.getStart());
-		  Date calcEnd = Utils.getMinimum(end, vacation.getEnd());
-		  if (calcStart.after(calcEnd)) {
-			  continue;
-		  }
-		  double myVacationHours = getVacationDays(calcStart, calcEnd) * 8;
-		  double othersWorkedHours = getWorkedHoursOfOneEmployeeWithoutMe(employeeStations, calcStart, calcEnd, allVacations);
-		  workedHoursDifferenceBetweenMeAndOthers += (othersWorkedHours - myVacationHours);
-		  vacationDays.addAll(Utils.getDatesBetweenTwoDates(calcStart, calcEnd));
-	  }
-	  
-	  int workDayIndex = 1;
-	  int totalWorkDaysThisMonth = 0;
-	  Calendar cal = Calendar.getInstance();
-	  cal.setTime(start);
-	  while (!cal.getTime().after(end)) {
-		  cal.add(Calendar.DAY_OF_MONTH, 1);
-		  if (!Utils.areDatesContainDate(vacationDays, cal.getTime())) {
-			  totalWorkDaysThisMonth++;
-		  }
-		  if (!cal.getTime().after(workDate)) {
-			  workDayIndex++;
-		  }
-	  }
-	  return workedHoursDifferenceBetweenMeAndOthers / totalWorkDaysThisMonth * workDayIndex * (-1);
+  private double getAuxWorkedHoursFromDifferences(Date workDate, EmployeeStatusModel employee, List<RuleVacationEntity> vacationsOfEmployee,
+                                                  List<RuleVacationEntity> allVacations) {
+    if (workDate == null || vacationsOfEmployee == null || vacationsOfEmployee.size() == 0) {
+      return 0;
+    }
+
+    double workedHoursDifferenceBetweenMeAndOthers = 0;
+    Date start = Utils.getDateFromBeginningOfMonth(workDate);
+    Date end = Utils.getDateAtEndOfMonth(workDate);
+    List<Date> vacationDays = new ArrayList<>();
+    List<StationEntity> employeeStations = stationRepository.getStationsOfEmployee(employee.getId());
+    for (RuleVacationEntity vacation : vacationsOfEmployee) {
+      Date calcStart = Utils.getMaximum(start, vacation.getStart());
+      Date calcEnd = Utils.getMinimum(end, vacation.getEnd());
+      if (calcStart.after(calcEnd)) {
+        continue;
+      }
+      double myVacationHours = getVacationDays(calcStart, calcEnd) * 8;
+      double othersWorkedHours = getWorkedHoursOfOneEmployeeWithoutMe(employeeStations, calcStart, calcEnd, allVacations);
+      workedHoursDifferenceBetweenMeAndOthers += (othersWorkedHours - myVacationHours);
+      vacationDays.addAll(Utils.getDatesBetweenTwoDates(calcStart, calcEnd));
+    }
+
+    int workDayIndex = 1;
+    int totalWorkDaysThisMonth = 0;
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(start);
+    while (!cal.getTime().after(end)) {
+      cal.add(Calendar.DAY_OF_MONTH, 1);
+      if (!Utils.areDatesContainDate(vacationDays, cal.getTime())) {
+        totalWorkDaysThisMonth++;
+      }
+      if (!cal.getTime().after(workDate)) {
+        workDayIndex++;
+      }
+    }
+    return workedHoursDifferenceBetweenMeAndOthers / totalWorkDaysThisMonth * workDayIndex * (-1);
   }
 
+  /**
+   * Gets the vacation days.
+   *
+   * @param calcStart the calc start
+   * @param calcEnd the calc end
+   * @return the vacation days
+   */
   private long getVacationDays(Date calcStart, Date calcEnd) {
-	if (calcStart == null || calcEnd == null || calcStart.after(calcEnd)) {
-		return 0;
-	}
-	long diff = Utils.getDateDifference(calcStart, calcEnd, TimeUnit.DAYS) + 1;
-	if (calcStart.getTime() != calcEnd.getTime()) {
-		diff++;
-	}
-	return diff;
+    if (calcStart == null || calcEnd == null || calcStart.after(calcEnd)) {
+      return 0;
+    }
+    long diff = Utils.getDateDifference(calcStart, calcEnd, TimeUnit.DAYS) + 1;
+    if (calcStart.getTime() != calcEnd.getTime()) {
+      diff++;
+    }
+    return diff;
   }
 
-	private double getWorkedHoursOfOneEmployeeWithoutMe(List<StationEntity> employeeStations, Date startInterval, Date endInterval, List<RuleVacationEntity> allVacations) {
-		if (employeeStations == null || startInterval == null || endInterval == null || allVacations == null) {
-			return 0;
-		}
-		
-		double result = 0;
-		double daysNumber = getVacationDays(startInterval, endInterval);
-		for (StationEntity station:employeeStations) {
-			int availableEmployeesNumber = getAvailableEmployeesNumberForStationWithinInterval(station, startInterval, endInterval, allVacations);
-			result += (station.getCapacity() * daysNumber * 24 / availableEmployeesNumber);
-		}
-		return result;
-	}
+  /**
+   * Gets the worked hours of one employee without me.
+   *
+   * @param employeeStations the employee stations
+   * @param startInterval the start interval
+   * @param endInterval the end interval
+   * @param allVacations the all vacations
+   * @return the worked hours of one employee without me
+   */
+  private double getWorkedHoursOfOneEmployeeWithoutMe(List<StationEntity> employeeStations, Date startInterval, Date endInterval,
+                                                      List<RuleVacationEntity> allVacations) {
+    if (employeeStations == null || startInterval == null || endInterval == null || allVacations == null) {
+      return 0;
+    }
 
-	private int getAvailableEmployeesNumberForStationWithinInterval(StationEntity station, Date startInterval, Date endInterval, List<RuleVacationEntity> allVacations) {
-		if (station == null || startInterval == null || endInterval == null) {
-			return 0;
-		}
-		
-		int availableEmployeesNumber = 0;
-		Set<EmployeeEntity> employeesWorkingInStation = getEmployeesWorkingInStationForInterval(station, startInterval, endInterval);
-		for (EmployeeEntity employee:employeesWorkingInStation) {
-			List<RuleVacationEntity> employeeVacations = getVacationsOfEmployee(employee.getId(), allVacations);
-			if (!isEmployeeOnVacationBetweenInterval(employeeVacations, startInterval, endInterval)) {
-				availableEmployeesNumber ++;
-			}
-		}
-		return availableEmployeesNumber;
-	}
+    double result = 0;
+    double daysNumber = getVacationDays(startInterval, endInterval);
+    for (StationEntity station : employeeStations) {
+      int availableEmployeesNumber = getAvailableEmployeesNumberForStationWithinInterval(station, startInterval, endInterval, allVacations);
+      result += (station.getCapacity() * daysNumber * 24 / availableEmployeesNumber);
+    }
+    return result;
+  }
 
-	private Set<EmployeeEntity> getEmployeesWorkingInStationForInterval(StationEntity station, Date startInterval, Date endInterval) {
-		List<RuleVacationEntity> vacations = ruleRepository.findActiveVacations(startInterval, endInterval);
-		if (vacations == null || vacations.size() == 0) {
-			return station.getEmployees();
-		}
-		
-		List<EmployeeEntity> replacers = new ArrayList<>();
-		for (RuleVacationEntity vacation:vacations) {
-			if (vacation.getReplacers() == null || vacation.getReplacers().size() == 0 || !doesEmployeeWorkInStation(vacation.getEmployees().iterator().next(), station)) {
-				continue;
-			}
-			replacers.addAll(vacation.getReplacers());
-		}
-		
-		replacers.addAll(station.getEmployees());
-		Set<EmployeeEntity> unique = replacers.stream().collect(collectingAndThen(toCollection(() -> new TreeSet<>(comparingLong(EmployeeEntity::getId))), HashSet::new));
-		return unique;
-	}
+  /**
+   * Gets the available employees number for station within interval.
+   *
+   * @param station the station
+   * @param startInterval the start interval
+   * @param endInterval the end interval
+   * @param allVacations the all vacations
+   * @return the available employees number for station within interval
+   */
+  private int getAvailableEmployeesNumberForStationWithinInterval(StationEntity station, Date startInterval, Date endInterval,
+                                                                  List<RuleVacationEntity> allVacations) {
+    if (station == null || startInterval == null || endInterval == null) {
+      return 0;
+    }
 
-	private boolean doesEmployeeWorkInStation(EmployeeEntity employee, StationEntity station) {
-		if (employee == null || station == null || employee.getStations() == null) {
-			return false;
-		}
-		for (StationEntity employeeStation:employee.getStations()) {
-			if (station.getId() == employeeStation.getId()) {
-				return true;
-			}
-		}
-		return false;
-	}
+    int availableEmployeesNumber = 0;
+    Set<EmployeeEntity> employeesWorkingInStation = getEmployeesWorkingInStationForInterval(station, startInterval, endInterval);
+    for (EmployeeEntity employee : employeesWorkingInStation) {
+      List<RuleVacationEntity> employeeVacations = getVacationsOfEmployee(employee.getId(), allVacations);
+      if (!isEmployeeOnVacationBetweenInterval(employeeVacations, startInterval, endInterval)) {
+        availableEmployeesNumber++;
+      }
+    }
+    return availableEmployeesNumber;
+  }
 
-	private boolean isEmployeeOnVacationBetweenInterval(List<RuleVacationEntity> employeeVacations, Date startInterval, Date endInterval) {
-		if (employeeVacations == null || startInterval == null || endInterval == null) {
-			return false;
-		}
-		for (RuleVacationEntity vacation:employeeVacations) {
-			Date calcStart = Utils.getMaximum(startInterval, vacation.getStart());
-			Date calcEnd = Utils.getMinimum(endInterval, vacation.getEnd());
-			if (calcStart.after(calcEnd)) {
-				continue;
-			}
-			return true;
-		}
-		return false;
-	}
+  /**
+   * Gets the employees working in station for interval.
+   *
+   * @param station the station
+   * @param startInterval the start interval
+   * @param endInterval the end interval
+   * @return the employees working in station for interval
+   */
+  private Set<EmployeeEntity> getEmployeesWorkingInStationForInterval(StationEntity station, Date startInterval, Date endInterval) {
+    List<RuleVacationEntity> vacations = ruleRepository.findActiveVacations(startInterval, endInterval);
+    if (vacations == null || vacations.size() == 0) {
+      return station.getEmployees();
+    }
+
+    List<EmployeeEntity> replacers = new ArrayList<>();
+    for (RuleVacationEntity vacation : vacations) {
+      if (vacation.getReplacers() == null || vacation.getReplacers().size() == 0 ||
+        !doesEmployeeWorkInStation(vacation.getEmployees().iterator().next(), station)) {
+        continue;
+      }
+      replacers.addAll(vacation.getReplacers());
+    }
+
+    replacers.addAll(station.getEmployees());
+    Set<EmployeeEntity> unique = replacers.stream()
+        .collect(collectingAndThen(toCollection(() -> new TreeSet<>(comparingLong(EmployeeEntity::getId))), HashSet::new));
+    return unique;
+  }
+
+  /**
+   * Does employee work in station.
+   *
+   * @param employee the employee
+   * @param station the station
+   * @return true, if successful
+   */
+  private boolean doesEmployeeWorkInStation(EmployeeEntity employee, StationEntity station) {
+    if (employee == null || station == null || employee.getStations() == null) {
+      return false;
+    }
+    for (StationEntity employeeStation : employee.getStations()) {
+      if (station.getId() == employeeStation.getId()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Checks if is employee on vacation between interval.
+   *
+   * @param employeeVacations the employee vacations
+   * @param startInterval the start interval
+   * @param endInterval the end interval
+   * @return true, if is employee on vacation between interval
+   */
+  private boolean isEmployeeOnVacationBetweenInterval(List<RuleVacationEntity> employeeVacations, Date startInterval, Date endInterval) {
+    if (employeeVacations == null || startInterval == null || endInterval == null) {
+      return false;
+    }
+    for (RuleVacationEntity vacation : employeeVacations) {
+      Date calcStart = Utils.getMaximum(startInterval, vacation.getStart());
+      Date calcEnd = Utils.getMinimum(endInterval, vacation.getEnd());
+      if (calcStart.after(calcEnd)) {
+        continue;
+      }
+      return true;
+    }
+    return false;
+  }
 
 }
